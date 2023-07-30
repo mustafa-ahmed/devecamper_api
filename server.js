0abfc83584e9cbcc419d45abf05cfaabc97aa20f
Moustafa Mohamed <maamohamed@alinma.com>
2023-07-30 16:37:37 +0300

    save changes

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const morgan = require("morgan");
-
 const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // load env variables
 dotenv.config({ path: "./config/config.env" });
+
+// connect databse
+connectDB();
 
 const app = express();
 
 // Dev logging middleware
 if (process.env.NODE_ENV == "development") {
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 }
 
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `server is running on port ${process.env.PORT} mode on port ${PORT}`
+    `server is running on port ${process.env.PORT} mode on port ${PORT}`.yellow.bold
   )
 );
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
